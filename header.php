3ef269a4e9dbcc18d23ff14a112afd57de78df35
Selvakumar Duraipandian <selvasudar3@gmail.com>
2025-06-15 10:23:41 +0530

    nav walker

--- header.php
+++ header.php
@@ -78,23 +78,6 @@
                                     'walker'          => new KF_Bootstrap_Navwalker(),
                                 ));
                             ?>
-
-                            <a href="index.html" class="nav-item nav-link active">Home</a>
-                            <a href="malathi" class="nav-item nav-link">About</a>
-                            <a href="service.html" class="nav-item nav-link">Service</a>
-                            <a href="blog.html" class="nav-item nav-link">Blog</a>
-                            
-                            <div class="nav-item dropdown">
-                                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                                <div class="dropdown-menu m-0">
-                                    <a href="feature.html" class="dropdown-item">Our Feature</a>
-                                    <a href="cars.html" class="dropdown-item">Our Cars</a>
-                                    <a href="team.html" class="dropdown-item">Our Team</a>
-                                    <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                                    <a href="404.html" class="dropdown-item">404 Page</a>
-                                </div>
-                            </div>
-                            <a href="contact.html" class="nav-item nav-link">Contact</a>
                         </div>
                         <a href="#" class="btn btn-primary rounded-pill py-2 px-4">Get Started</a>
                     </div>
